52a393a2e2ef448a7b5dbc0dc22cb7fb6762199b
gaarahan <gaarahan@foxmail.com>
2020-08-05 18:53:30 +0800

    [Gaarahan] Refactor practice 4

--- 4/src/main/java/com/twu/refactoring/NumberCruncher.java
+++ 4/src/main/java/com/twu/refactoring/NumberCruncher.java
@@ -1,41 +1,43 @@
 package com.twu.refactoring;
 
+import java.util.Arrays;
+
 public class NumberCruncher {
-    private final int[] numbers;
+  private final int[] numbers;
 
-    public NumberCruncher(int... numbers) {
-        this.numbers = numbers;
-    }
+  public NumberCruncher(int... numbers) {
+    this.numbers = numbers;
+  }
 
-    public int countEven() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) count++;
-        }
-        return count;
-    }
+  public int countEven() {
+    return (int) Arrays.stream(numbers).filter(this::isEven).count();
+  }
 
-    public int countOdd() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 1) count++;
-        }
-        return count;
-    }
+  public int countOdd() {
+    return (int) Arrays.stream(numbers).filter(this::isOdd).count();
+  }
 
-    public int countPositive() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number >= 0) count++;
-        }
-        return count;
-    }
+  public int countPositive() {
+    return (int) Arrays.stream(numbers).filter(this::isPositive).count();
+  }
 
-    public int countNegative() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number < 0) count++;
-        }
-        return count;
-    }
+  public int countNegative() {
+    return (int) Arrays.stream(numbers).filter(this::isNegative).count();
+  }
+
+  private boolean isEven(int num) {
+    return num % 2 == 0;
+  }
+
+  private boolean isOdd(int num) {
+    return num % 2 == 1;
+  }
+
+  private boolean isPositive(int num) {
+    return num >= 0;
+  }
+
+  private boolean isNegative(int num) {
+    return num < 0;
+  }
 }
